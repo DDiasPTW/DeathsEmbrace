fce5d3130c7dcfcf7d50d80086170be83ecd2402
Dias <dmpldias@gmail.com>
2021-06-14 18:03:54 +0100

    Woop
    +Level 10
    +Tutorial
    +QOL Changes

---
+++ Death's Embrace/Assets/TutorialZone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialZone : MonoBehaviour
+{
+    public GameObject Tut_UI;
+    public Text Tut_Text;
+    public string textTut;
+    private void Awake()
+    {
+        Tut_UI.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Tut_Text.text = textTut;
+            Tut_UI.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Tut_UI.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
